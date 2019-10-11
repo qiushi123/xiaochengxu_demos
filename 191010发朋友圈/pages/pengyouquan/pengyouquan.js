11fda4879cf4e7e6e9c0d799b1db034e6153d68d
qiushi123 <2501902696@qq.com>
2019-10-11 10:42:44 +0800

    小程序模仿微信发朋友圈

---
+++ 191010发朋友圈/pages/pengyouquan/pengyouquan.js
@@ -0,0 +1,48 @@
+// pages/pengyouquan/pengyouquan.js
+Page({
+  data: {
+    imgList: []
+  },
+
+  onLoad: function(options) {
+
+  },
+
+  //选择图片
+  ChooseImage() {
+    wx.chooseImage({
+      count: 8, //默认9
+      sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album'], //从相册选择
+      success: (res) => {
+        console.log("选择图片成功", res)
+        if (this.data.imgList.length != 0) {
+          this.setData({
+            imgList: this.data.imgList.concat(res.tempFilePaths)
+          })
+        } else {
+          this.setData({
+            imgList: res.tempFilePaths
+          })
+        }
+      }
+    });
+  },
+  //删除图片
+  DeleteImg(e) {
+    wx.showModal({
+      title: '要删除这张照片吗？',
+      content: '',
+      cancelText: '取消',
+      confirmText: '确定',
+      success: res => {
+        if (res.confirm) {
+          this.data.imgList.splice(e.currentTarget.dataset.index, 1);
+          this.setData({
+            imgList: this.data.imgList
+          })
+        }
+      }
+    })
+  },
+})
